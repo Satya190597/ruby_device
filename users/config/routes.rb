fd282b437d158ed95c563c8dd94a8296bb6b2bac
satya <satya.nandy@evossolutions.com>
2018-04-12 10:42:30 +0530

    Add root path to my welcome page -> root welcome#index

--- users/config/routes.rb
+++ users/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# Adding root path to my welcome page
+	root 'welcome#index'
+	get 'welcome/index'
+	devise_for :users
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
